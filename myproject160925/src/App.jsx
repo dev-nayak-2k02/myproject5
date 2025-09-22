4db4a32c6214c8062f509f90a1f0fe32c7eb853c
dev nayak10 <devprasannanayak@gmail.com>
2025-09-22 20:47:18 +0530

    content added

--- myproject160925/src/App.jsx
+++ myproject160925/src/App.jsx
@@ -9,8 +9,11 @@ function App() {
   return (
     <>
       <form onSubmit={handleSubmit}>
-        <input type="text" value={val} onChange={(e)=>setval(e.target.value)}/>
-        <h1>{val}</h1>
+        <input type="text" 
+        value={val} 
+        onChange={(e)=>setVal(e.target.value)
+        }/>
+        <h1>{count}</h1>
         <button type="submit">click me to increase</button>
       </form>
     </>
