572f81a34e449cfe6182b0f8f3f578bad0723ca0
dev nayak10 <devprasannanayak@gmail.com>
2025-09-22 10:31:15 +0530

    content added in App.jsx

--- myproject210925/src/App.jsx
+++ myproject210925/src/App.jsx
@@ -1,16 +1,15 @@
 import { useState } from "react"
 function App() {
-  const[count,setCount] = useState(0)
-  const[val, setVal] = useState(1)
-  const handleChange=(e)=>{
-    setVal(e.target.value)
-  }
-
+  const [count,setCount] = useState(0);
+  const [val,setVal] = useState(1);
   return (
     <>
-      <input type="text" value={val} onChange={handleChange}/>
+      <input type="text" value={val} onChange={(e)=>{setVal(e.target.value)}}/>
       <h1>{count}</h1>
-      <button onClick={()=> setCount(count+Number(val))}>Click me! to increase</button>
+      <button onClick={(e)=>{
+        setCount(count+Number(val))
+        console.log(e)
+      }}>Click to Increase</button>
     </>
   )
 }
