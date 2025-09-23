ed5f8c22733b046c0cc1978e70e379dbf360936e
dev nayak10 <devprasannanayak@gmail.com>
2025-09-23 12:00:12 +0530

    content added

--- myproject160925/src/App.jsx
+++ myproject160925/src/App.jsx
@@ -1,20 +1,20 @@
-import { useState } from "react"
-function App() {
-  const [val,setVal] = useState(1)
-  const [count,setCount] = useState(0)
-  const handleSubmit =(e)=>{
+import { useState } from "react";
+function App(){
+  const[count,setCount] = useState(0)
+  const[val,setVal] = useState(1)
+  const handleSubmit=(e)=>{
     e.preventDefault();
-    setCount(count + Number(val))    
+    setCount(count + Number(val));
   }
-  return (
+  return(
     <>
       <form onSubmit={handleSubmit}>
         <input type="text" 
-        value={val} 
-        onChange={(e)=>setVal(e.target.value)
-        }/>
+        value={val}
+        onChange={(e)=>setVal(e.target.value)}/>
+
         <h1>{count}</h1>
-        <button type="submit">click me to increase</button>
+        <button type="submit">Increase</button>
       </form>
     </>
   )
