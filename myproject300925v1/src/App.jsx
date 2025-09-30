bc84360c183798720cff970b36f89997cc2366a1
dev nayak10 <devprasannanayak@gmail.com>
2025-09-30 21:21:01 +0530

    content added

--- myproject300925v1/src/App.jsx
+++ myproject300925v1/src/App.jsx
@@ -1,10 +1,11 @@
+import StudentDetails from "./StudentDetails";
 function App() {
   const name = "Dev Prasanna Nayak";
   const roll = 101
   const marks = [97,99,91];
   return (
     <>
-      
+      <StudentDetails name = {name} roll = {roll} marks = {marks}/>
     </>
   )
 }
--- myproject300925v1/src/StudentDetails.jsx
+++ myproject300925v1/src/StudentDetails.jsx
@@ -1,8 +1,14 @@
-export default function StudentDetails(){
-    
-    return(
-        <div>
-            <h1></h1>
-        </div>
-    )
+export default function StudentDetails({ name, roll, marks }) {
+  return (
+    <div>
+      <h1>Name = {name}</h1>
+      <h1>roll = {roll}</h1>
+      <h1>
+        marks =
+        {marks.map((mark) => {
+          return <span> {mark} </span>;
+        })}
+      </h1>
+    </div>
+  );
 }
