fa7574087b36f2432895121c6585a7c7b0c90de3
dev nayak10 <devprasannanayak@gmail.com>
2025-10-01 10:42:09 +0530

    content added

--- myproject011025v2/src/components/Todo.jsx
+++ myproject011025v2/src/components/Todo.jsx
@@ -11,7 +11,7 @@ export default function Todo() {
   return (
     <>
       <form onSubmit={handleSubmit}>
-        <input type="text" value={todo} />
+        <input type="text" value={todo} onChange={(e)=>setTodo(e.target.value)}/>
         <button type="submit">ADD</button>
       </form>
       {todos.map((item)=>(<TodoItem key={item} item = {item}/>))}
