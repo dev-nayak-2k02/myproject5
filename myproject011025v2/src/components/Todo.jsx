fbf393e3a16781c0dd3742c3a92f0cf370d5c6f0
dev nayak10 <devprasannanayak@gmail.com>
2025-10-01 10:38:08 +0530

    content added

---
+++ myproject011025v2/src/components/Todo.jsx
@@ -0,0 +1,20 @@
+import { useState } from "react";
+import TodoItem from "../../../myproject011025v1/src/components/TodoItem";
+export default function Todo() {
+    const[todo, setTodo] = useState("")
+    const[todos, setTodos] = useState([])
+    function handleSubmit(e){
+        e.preventDefault();
+        setTodos([...todos,todo])
+        setTodo('')
+    }
+  return (
+    <>
+      <form onSubmit={handleSubmit}>
+        <input type="text" value={todo} />
+        <button type="submit">ADD</button>
+      </form>
+      {todos.map((item)=>(<TodoItem key={item} item = {item}/>))}
+    </>
+  );
+}
