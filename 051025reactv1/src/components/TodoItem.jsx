564bb4f8a206f0bb80f47174cb9733ec40b56e8f
dev nayak10 <devprasannanayak@gmail.com>
2025-10-05 20:20:28 +0530

    content added

--- 051025reactv1/src/components/TodoItem.jsx
+++ 051025reactv1/src/components/TodoItem.jsx
@@ -1,3 +1,19 @@
-export default function TodoItem({item}) {
-    return <h1>{item}</h1>
+export default function TodoItem({ item, todos, setTodos }) {
+  function handleDelete(item) {
+    setTodos(todos.filter((todo) => todo !== todo));
+  }
+  return (
+    <h1>
+      {item}
+      <span>
+        <button
+          onClick={() => {
+            handleDelete(item);
+          }}
+        >
+          X
+        </button>
+      </span>
+    </h1>
+  );
 }
