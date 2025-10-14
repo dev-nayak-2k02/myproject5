6020d4b74ab803f30d2474cd99b4b07d76a96361
dev nayak10 <devprasannanayak@gmail.com>
2025-10-14 17:53:36 +0530

    content added

---
+++ 141025reactv1/src/components/Form.jsx
@@ -0,0 +1,12 @@
+import { useState } from "react";
+
+export default function Todo() {
+  return (
+    <>
+      <form action="">
+        <input type="text" />
+        <button></button>
+      </form>
+    </>
+  );
+}
