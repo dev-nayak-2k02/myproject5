b5c979bbf639b7bd3c59ef6178f019a4f14f4ba5
dev nayak10 <devprasannanayak@gmail.com>
2025-10-24 21:18:27 +0530

    content added

--- 191025reactv1/src/components/TitleHandler.jsx
+++ 191025reactv1/src/components/TitleHandler.jsx
@@ -1,9 +1,11 @@
 import { useState } from "react";
+import DataDisplay from "./DataDisplay";
 function TitleHandler({ name, setName }){
     const[title,setTitle] = useState();
     function addNameTitle(){
         setTitle(title)
         setName(`${name} ${title}`);
+        <DataDisplay />
     }
     return(
         <>
