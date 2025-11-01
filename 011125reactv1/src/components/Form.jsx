0e23f91b75909df9c2aaf5a08e1bfbc093577c0b
dev nayak10 <devprasannanayak@gmail.com>
2025-11-01 18:43:55 +0530

    content added

---
+++ 011125reactv1/src/components/Form.jsx
@@ -0,0 +1,12 @@
+import { useState } from "react";
+
+export default function Form() {
+  return (
+    <>
+      <form action="">
+        <input type="text" />
+        <button></button>
+      </form>
+    </>
+  );
+}
---
+++ 011125reactv1/src/components/Todo.jsx
@@ -0,0 +1,12 @@
+import { useState } from "react";
+
+function Todo(){
+    const[todos,setTodos] = useState([]);
+    return(
+        <>
+        
+        </>
+    )
+}
+
+export default Todo
