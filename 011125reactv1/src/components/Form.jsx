1969b0c62350db16ccff82a2a19476610cb5903a
dev nayak10 <devprasannanayak@gmail.com>
2025-11-01 18:55:43 +0530

    content added

--- 011125reactv1/src/components/Form.jsx
+++ 011125reactv1/src/components/Form.jsx
@@ -1,16 +1,22 @@
 import { useState } from "react";
 
-export default function Form() {
-
-    const[todo,setTodo] = useState('');
-    function handleSubmit(e){
-        e.preventDefault();
-        
-    }
+export default function Form({ todos, setTodos }) {
+  const [todo, setTodo] = useState("");
+  function handleSubmit(e) {
+    e.preventDefault();
+    setTodos([...todos,todo]);
+    setTodo('');
+  }
   return (
     <>
       <form onSubmit={handleSubmit}>
-        <input type="text" value={todo} onChange={(e)=>{setTodo(e.target.value)}}/>
+        <input
+          type="text"
+          value={todo}
+          onChange={(e) => {
+            setTodo(e.target.value);
+          }}
+        />
         <button type="submit">ADD</button>
       </form>
     </>
--- 011125reactv1/src/components/Todo.jsx
+++ 011125reactv1/src/components/Todo.jsx
@@ -1,12 +1,13 @@
 import { useState } from "react";
+import Form from "./Form";
 
-function Todo(){
-    const[todos,setTodos] = useState([]);
-    return(
-        <>
-        
-        </>
-    )
+function Todo() {
+  const [todos, setTodos] = useState([]);
+  return (
+    <>
+      <Form todos={todos} setTodos={setTodos} />
+    </>
+  );
 }
 
-export default Todo
+export default Todo;
