62ac4d6cd50e56ca63d484ca2b32e2e5817ac8bf
dev nayak10 <devprasannanayak@gmail.com>
2025-11-01 18:48:38 +0530

    content added

--- 011125reactv1/src/components/Form.jsx
+++ 011125reactv1/src/components/Form.jsx
@@ -1,11 +1,17 @@
 import { useState } from "react";
 
 export default function Form() {
+
+    const[todo,setTodo] = useState('');
+    function handleSubmit(e){
+        e.preventDefault();
+        
+    }
   return (
     <>
-      <form action="">
-        <input type="text" />
-        <button></button>
+      <form onSubmit={handleSubmit}>
+        <input type="text" value={todo} onChange={(e)=>{setTodo(e.target.value)}}/>
+        <button type="submit">ADD</button>
       </form>
     </>
   );
