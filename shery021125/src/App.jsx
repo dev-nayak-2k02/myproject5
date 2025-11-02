7b49083a8f78e7b68b16f75e98776f217e4fb01e
dev nayak10 <devprasannanayak@gmail.com>
2025-11-02 22:27:35 +0530

    content added

--- shery021125/src/App.jsx
+++ shery021125/src/App.jsx
@@ -1,8 +1,21 @@
 import React from 'react'
 
 const App = () => {
+  const handleClick = () =>{
+    alert('clicked Saaar!!')
+  }
+  const handleParameterizedFunction = (msg)=>{
+    alert(msg);
+  }
+  // const wrapperFunction = () =>{
+  //   handleParameterizedFunction('Kujo rodharooooooo');
+  // }
   return (
-    <div>App</div>
+    <>
+      <div>App</div>
+      <button onClick={handleClick}>click</button>
+      <button onClick={()=>handleParameterizedFunction('Rodharoooo.....')}>click(param)</button>
+    </>
   )
 }
 
