aa51e94e744038e29e82eb80c5991f6bf1c0e800
dev nayak10 <devprasannanayak@gmail.com>
2025-11-05 12:04:26 +0530

    content added

--- 051125reactpracticev2/src/components/Counter.jsx
+++ 051125reactpracticev2/src/components/Counter.jsx
@@ -2,17 +2,10 @@ import { useState } from "react"
 
 export default function Counter(){
     const[count, setCount] = useState(0);
-    function handleCounter(){
-        incrementer(setCount)
-    }
-
-    function incrementer(fn){
-        fn(count+1);
-    }
     return(
         <>
             <h1>{count}</h1>
-            <button onClick={handleCounter}>Add</button>
+            <button onClick={()=> setCount(count+1)}>Add</button>
         </>
     )
 }
