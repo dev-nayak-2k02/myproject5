bcc468d8d01ebbc3fd5f3b1f3b8c4d38eed940c1
dev nayak10 <devprasannanayak@gmail.com>
2025-11-05 09:53:01 +0530

    content added

--- 051125reactpracticev1/src/components/Header.jsx
+++ 051125reactpracticev1/src/components/Header.jsx
@@ -1,11 +1,11 @@
-export default function Student(props){
-    let[name, gender] = [props.name, props.gender]
+export default function Student({name, gender}){
+    
     // let name = 'Devu granadi';
     // let gender = 'male';
     return(
         <>
-            <h1>Name: {props.name}</h1>
-            <h1>Gender: {props.gender}</h1>
+            <h1>Name: {name}</h1>
+            <h1>Gender: {gender}</h1>
         </>
     )
 }
