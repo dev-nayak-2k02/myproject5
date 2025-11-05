5c420a04902f032593b55693ec59c67aa456e658
dev nayak10 <devprasannanayak@gmail.com>
2025-11-05 09:26:11 +0530

    content added

--- 051125reactpracticev1/src/components/Student.jsx
+++ 051125reactpracticev1/src/components/Student.jsx
@@ -1,11 +1,16 @@
 export default function Student(){
+    let name = 'Devu granadi';
+    let gender = 'male';
+    let age = 19;
+    let isVoting = true;
+    let height = 5.9;
     return(
         <>
-            <h1>Name: Dev Nayak</h1>
-            <h1>Gender: Male</h1>
-            <h1>Age: 17</h1>
-            <h1>Voting age: true</h1>
-            <h1>height: 5.3</h1>
+            <h1>Name: {name}</h1>
+            <h1>Gender: {gender}</h1>
+            <h1>Age: {age}</h1>
+            <h1>Voting age: {isVoting}</h1>
+            <h1>height: {height}</h1>
         </>
     )
 }
