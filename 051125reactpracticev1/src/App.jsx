89de8b0d7adebade9854609c19277d34c59710d5
dev nayak10 <devprasannanayak@gmail.com>
2025-11-05 09:22:27 +0530

    content added

--- 051125reactpracticev1/src/App.jsx
+++ 051125reactpracticev1/src/App.jsx
@@ -1,11 +1,9 @@
-
+import Student from "./components/Student"
 function App() {
-
   return (
     <>
-      
+      <Student />
     </>
   )
 }
-
 export default App
---
+++ 051125reactpracticev1/src/components/Student.jsx
@@ -0,0 +1,11 @@
+export default function Student(){
+    return(
+        <>
+            <h1>Name: Dev Nayak</h1>
+            <h1>Gender: Male</h1>
+            <h1>Age: 17</h1>
+            <h1>Voting age: true</h1>
+            <h1>height: 5.3</h1>
+        </>
+    )
+}
