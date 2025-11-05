af60b01ee2953b567ed6baaa28725a7d2b81c053
dev nayak10 <devprasannanayak@gmail.com>
2025-11-05 11:57:06 +0530

    content added

---
+++ 051125reactpracticev2/src/components/Counter.jsx
@@ -0,0 +1,14 @@
+import { useState } from "react"
+
+export default function Counter(){
+    const[count, setCount] = useState(0);
+    function handleCounter(){
+        setCount(count + 1);
+    }
+    return(
+        <>
+            <h1>{count}</h1>
+            <button onClick={handleCounter}>Add</button>
+        </>
+    )
+}
