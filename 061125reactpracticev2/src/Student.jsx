69afa06d00a990692ae81a87830be357ba161466
dev nayak10 <devprasannanayak@gmail.com>
2025-11-06 12:29:40 +0530

    content added

--- 061125reactpracticev2/src/Student.jsx
+++ 061125reactpracticev2/src/Student.jsx
@@ -1,7 +1,12 @@
-export default function Student(){
-    return(
-        <div>
-            
-        </div>
-    )
+export default function Student({ studentData }) {
+  const { name, clas, marks } = studentData;
+  return(
+    <div>
+        <h2>Name: {name}</h2>
+        <h2>class: {clas}</h2>
+        {marks.map((res)=>{
+            return <li key={res}>{res}</li>
+        })}
+    </div>
+  )
 }
