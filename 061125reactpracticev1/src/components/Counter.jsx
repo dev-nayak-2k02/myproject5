4567d8bc7f51813b3f4ba565f53b655eeeba4539
dev nayak10 <devprasannanayak@gmail.com>
2025-11-06 09:51:42 +0530

    content added

--- 061125reactpracticev1/src/components/Counter.jsx
+++ 061125reactpracticev1/src/components/Counter.jsx
@@ -5,7 +5,7 @@ export default function Counter(){
     const[val, setVal] = useState(1);
     return(
         <>
-            <input type="text" placeholder="0"/>
+            <input type="text" placeholder="0" onChange={(e)=>setCount(count + Number(e.target.value))}/>
             <h3>{count}</h3>
             <input type="text" value={val} onChange={(e)=>setVal(e.target.value)}/>
             <button onClick={(e)=>setCount(count + Number(val))}>Add</button>
