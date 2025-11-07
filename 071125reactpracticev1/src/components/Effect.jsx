f53660c9b394b792ed66b14fa8148d1b4476d93c
dev nayak10 <devprasannanayak@gmail.com>
2025-11-07 10:42:19 +0530

    content added

--- 071125reactpracticev1/src/components/Effect.jsx
+++ 071125reactpracticev1/src/components/Effect.jsx
@@ -2,10 +2,15 @@ import { useEffect, useState } from "react";
 
 export default function Effect(){
     const[count, setCount] = useState(0)
+    useEffect(()=>{
+        ()=>{
+            console.log('Effect is triggered');
+        }
+    },[count])
     return(
         <>
             <h2>{count}</h2>
-            <button onClick={setCount(count + 1)}>Add</button>
+            <button onClick={setCount(prev=>prev+1)}>Add</button>
         </>
     )
 }
