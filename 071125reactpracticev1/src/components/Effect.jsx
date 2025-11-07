108a65f256e6890b05e417cf8afd76dce6a3b51a
dev nayak10 <devprasannanayak@gmail.com>
2025-11-07 16:21:05 +0530

    content added

--- 071125reactpracticev1/src/components/Effect.jsx
+++ 071125reactpracticev1/src/components/Effect.jsx
@@ -1,14 +1,16 @@
+const URL = 'https://api.spoonacular.com/recipes/complexSearch';
+const API_KEY = 'ded7bbedd88a4be6b31d943077c0faac';
 import { useEffect, useState } from "react";
 
 export default function Effect() {
   const [query, setQuery] = useState("pizza");
   useEffect(()=>{
     async function getFoodData() {
-        const result = await fetch ('https://api.spoonacular.com/recipes/complexSearch?query=pasta&apiKey=ded7bbedd88a4be6b31d943077c0faac');
+        const result = await fetch (`${URL}?query=${query}&apiKey=${API_KEY}`);
         const data = await result.json();
-        console.log(data);
+        console.log(data.results);
     }
-    getFoodData()
+    getFoodData();
   },[query])
   return (
     <>
