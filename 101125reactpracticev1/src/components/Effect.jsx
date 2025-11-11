34674d13d9d9f5b9f5f7f9991574e9f5652611cd
dev nayak10 <devprasannanayak@gmail.com>
2025-11-11 08:52:58 +0530

    content added

--- 101125reactpracticev1/src/components/Effect.jsx
+++ 101125reactpracticev1/src/components/Effect.jsx
@@ -1,5 +1,5 @@
 let URL = "https://api.spoonacular.com/recipes/complexSearch";
-let API_KEY = "358ac2f11c4c40759687f76810d9449f";
+let API_KEY = "ded7bbedd88a4be6b31d943077c0faac";
 import { useEffect, useState } from "react";
 
 export default function Effect() {
@@ -11,7 +11,7 @@ export default function Effect() {
       let data = await fetch(`${URL}?query=${query}&apiKey=${API_KEY}`);
       let result = await data.json();
       // setItem(result.results[0].image)
-      // console.log(result.results[0]);
+      console.log(result.results[0]);
       setFoodData(result.results);
     }
     getFoodData();
@@ -26,7 +26,15 @@ export default function Effect() {
       />
       {
         foodData.map((foodItem)=>{
-          <h1>{foodItem.title}</h1>
+          return (
+
+            <div key={foodItem.id}>
+
+              <h1>{foodItem.title}</h1>
+              <img src={foodItem.image} alt={foodItem.title} />
+              <p>{foodItem.id}</p>
+            </div>
+          )
         })
       }
     </>
