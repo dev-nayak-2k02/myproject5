f2f7bbfff05eb88d543da2298a7ed354c3d7b747
dev nayak10 <devprasannanayak@gmail.com>
2025-11-12 18:39:36 +0530

    content added

--- 121125reactpracticev1/src/App.jsx
+++ 121125reactpracticev1/src/App.jsx
@@ -1,8 +1,11 @@
-function App() {
+import { useState } from "react"
+import Search from "./components/Search"
 
+function App() {
+  const[item,setItem] = useState([]);
   return (
     <>
-      
+      <Search setItem={setItem}/>
     </>
   )
 }
