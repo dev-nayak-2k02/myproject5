0e1878c26015b788e014e03c0125588ceb1cd390
dev nayak10 <devprasannanayak@gmail.com>
2025-11-13 18:32:13 +0530

    content added

--- 131125reactpracticev1/src/components/ItemDisplay.jsx
+++ 131125reactpracticev1/src/components/ItemDisplay.jsx
@@ -1,7 +1,11 @@
-export default function ItemDisplay(){
+export default function ItemDisplay({res}){
     return(
         <>
-            
+            <div>
+                <h1>{res.id}</h1>
+                <img src={res.image} alt={res.image} />
+                <p>{res.description}</p>
+            </div>
         </>
     )
 }
--- 131125reactpracticev1/src/components/ItemList.jsx
+++ 131125reactpracticev1/src/components/ItemList.jsx
@@ -1,9 +1,10 @@
+import ItemDisplay from "./ItemDisplay";
 export default function ItemList({item}){
     return(
         <>
             {
                 item.map((res)=>(
-                    
+                    <ItemDisplay key={res.id} res={res}/>
                 ))
             }
         </>
