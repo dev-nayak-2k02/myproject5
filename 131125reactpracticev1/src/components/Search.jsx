f3b704c2cd8a8ac1d6a439dc10b9b62fbbea437b
dev nayak10 <devprasannanayak@gmail.com>
2025-11-13 18:13:51 +0530

    content added

--- 131125reactpracticev1/src/components/Search.jsx
+++ 131125reactpracticev1/src/components/Search.jsx
@@ -1,19 +1,22 @@
 import { useEffect, useState } from "react";
-
-let URL = 'https://fakestoreapi.com/products?id=integer&title=string&price=number <float>';
-export default function Search(){
-    const[query,setQuery] = useState('title');
-    useEffect(()=>{
-        async function getItem() {
-            let data = await fetch(`${URL}`);
-            let result = await data.json();
-
-        }
-        getItem();
-    },[query])
-    return(
-        <>
-            <input type="text" />
-        </>
-    )
+let URL =
+  "https://fakestoreapi.com/products?id=integer&title=string&price=number <float>";
+export default function Search() {
+  const [query, setQuery] = useState("title");
+  useEffect(() => {
+    async function getItem() {
+      let data = await fetch(`${URL}`);
+      let result = await data.json();
+    }
+    getItem();
+  }, [query]);
+  return (
+    <>
+      <input
+        type="text"
+        value={query}
+        onChange={(e) => setQuery(e.target.value)}
+      />
+    </>
+  );
 }
