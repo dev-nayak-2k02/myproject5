6ad853229f92b0ddfdeca31d42684d469622d317
dev nayak10 <devprasannanayak@gmail.com>
2025-11-14 16:55:50 +0530

    content added

--- 141125reactpracticev1/src/components/FoodDisplay.jsx
+++ 141125reactpracticev1/src/components/FoodDisplay.jsx
@@ -1,7 +1,10 @@
 export default function FoodDisplay({item}){
     return(
         <div>
-
+            <h1>{item.id}</h1>
+            <h2>{item.title}</h2>
+            <img src={item.image} alt={item.image} />
+            <p>{item.description}</p>
         </div>
     )
 }
