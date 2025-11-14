4ea1b52126b1a9a2de192d397c2fe619aa74c743
dev nayak10 <devprasannanayak@gmail.com>
2025-11-14 16:49:12 +0530

    content added

--- 141125reactpracticev1/src/components/Search.jsx
+++ 141125reactpracticev1/src/components/Search.jsx
@@ -1,11 +1,13 @@
+let URL = 'https://fakestoreapi.com/products?id=integer&title=string&price=number <float>';
 import { useEffect, useState } from "react";
 
-export default function Search() {
+export default function Search({setItem}) {
   const [query, setQuery] = useState("title");
   useEffect(() => {
     async function getItemDetails() {
-      let data = await fetch(``);
+      let data = await fetch(`${URL}`);
       let res = await data.json();
+      setItem(res);
     }
     getItemDetails();
   }, [query]);
