362dbbecce2c9b1e1bcca3e6bf499c7e4c9293f1
dev nayak10 <devprasannanayak@gmail.com>
2025-11-14 16:51:05 +0530

    content added

--- 141125reactpracticev1/src/App.jsx
+++ 141125reactpracticev1/src/App.jsx
@@ -1,10 +1,12 @@
 import { useState } from "react"
 import Search from "./components/Search"
+import FoodList from "./components/FoodList";
 function App() {
   const[item,setItem] = useState([]);
   return (
     <>
       <Search setItem={setItem}/>
+      <FoodList item={item}/>
     </>
   )
 }
