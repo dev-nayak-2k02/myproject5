893619441b0f82f0a6ad10697728d78af749ad8b
dev nayak10 <devprasannanayak@gmail.com>
2025-11-16 14:50:34 +0530

    content added

--- Todoapp3/src/components/Todo.jsx
+++ Todoapp3/src/components/Todo.jsx
@@ -2,12 +2,14 @@ import { useState } from "react";
 
 import React from 'react'
 import Form from "./Form";
+import TodoList from "./TodoList";
 
 const Todo = () => {
   const[todos,setTodos] = useState([]);
   return (
     <div>
         <Form todos={todos} setTodos={setTodos}/>
+        <TodoList todos={todos}/>
     </div>
   )
 }
---
+++ Todoapp3/src/components/TodoItem.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const TodoItem = ({item}) => {
+  return (
+    <div>
+        <h1>{item}</h1>
+    </div>
+  )
+}
+
+export default TodoItem
