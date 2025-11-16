fe8a857ba9b3a49cfbd5d4c861088f0e3a16004e
dev nayak10 <devprasannanayak@gmail.com>
2025-11-16 14:58:27 +0530

    content added

--- Todoapp3/src/components/TodoItem.jsx
+++ Todoapp3/src/components/TodoItem.jsx
@@ -1,11 +1,19 @@
-import React from 'react'
+import React from "react";
 
-const TodoItem = ({item}) => {
+const TodoItem = ({ item }) => {
+    function handleDelete(){
+        console.log('button clicked');
+    }
   return (
     <div>
-        <h1>{item}</h1>
+      <h1>
+        {item}
+        <span>
+          <button onClick={()=>handleDelete()}>X</button>
+        </span>
+      </h1>
     </div>
-  )
-}
+  );
+};
 
-export default TodoItem
+export default TodoItem;
