cc049d719d6bce87c20810ab734a26f00c93c94d
dev nayak10 <devprasannanayak@gmail.com>
2025-11-17 09:03:09 +0530

    content added

--- 171125reactpracticev1/src/components/ItemDisplay.jsx
+++ 171125reactpracticev1/src/components/ItemDisplay.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
 
-const ItemDisplay = () => {
+const ItemDisplay = ({item}) => {
   return (
     <div>
-        
+        <h1>{item.id}</h1>
+        <h2>{item.title}</h2>
+        <img src={item.image} alt={item.image} />
+        <p>{item.description}</p>
     </div>
   )
 }
