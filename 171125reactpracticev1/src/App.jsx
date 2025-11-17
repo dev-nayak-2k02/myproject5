2e90f0a71a434e542cc74ad732579abeffa93089
dev nayak10 <devprasannanayak@gmail.com>
2025-11-17 08:56:39 +0530

    content added

--- 171125reactpracticev1/src/App.jsx
+++ 171125reactpracticev1/src/App.jsx
@@ -1,11 +1,13 @@
 import { useState } from "react";
 import Search from "./components/Search";
+import ItemList from "./components/ItemList";
 
 function App() {
   const [item, setItem] = useState([]);
   return (
     <>
       <Search setItem={setItem} />
+      <ItemList item={item}/>
     </>
   );
 }
---
+++ 171125reactpracticev1/src/components/ItemList.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const ItemList = ({item}) => {
+  return (
+    <div>
+        
+    </div>
+  )
+}
+
+export default ItemList
