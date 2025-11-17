15b70b66070bda3d0f12a168e88abd1426913259
dev nayak10 <devprasannanayak@gmail.com>
2025-11-17 09:10:37 +0530

    content added

--- 171125reactpracticev1/src/components/ItemDisplay.jsx
+++ 171125reactpracticev1/src/components/ItemDisplay.jsx
@@ -1,8 +1,8 @@
-import React from 'react'
-
+import React from 'react';
+import './item.css';
 const ItemDisplay = ({item}) => {
   return (
-    <div>
+    <div className='card'>
         <h1>{item.id}</h1>
         <h2>{item.title}</h2>
         <img src={item.image} alt={item.image} />
