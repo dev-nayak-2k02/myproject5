c230f8ea25fc3b985af29e92d3b7f45e88599ea8
dev nayak10 <devprasannanayak@gmail.com>
2025-11-17 09:00:48 +0530

    content added

---
+++ 171125reactpracticev1/src/components/ItemDisplay.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const ItemDisplay = () => {
+  return (
+    <div>
+        
+    </div>
+  )
+}
+
+export default ItemDisplay
--- 171125reactpracticev1/src/components/ItemList.jsx
+++ 171125reactpracticev1/src/components/ItemList.jsx
@@ -1,9 +1,14 @@
 import React from 'react'
+import ItemDisplay from './ItemDisplay'
 
 const ItemList = ({item}) => {
   return (
     <div>
-        
+        {
+            item.map((item)=>(
+                <ItemDisplay key={item.id} item={item}/>
+            ))
+        }
     </div>
   )
 }
