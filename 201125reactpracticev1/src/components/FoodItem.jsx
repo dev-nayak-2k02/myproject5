5ab583b8bc8cc2c88518ef1844a44d6dbe30a5f9
dev nayak10 <devprasannanayak@gmail.com>
2025-11-20 09:01:06 +0530

    content added

--- 201125reactpracticev1/src/components/FoodItem.jsx
+++ 201125reactpracticev1/src/components/FoodItem.jsx
@@ -1,9 +1,18 @@
+import { useState } from "react"
+
 export default function FoodItem({item}){
+    const[hold,setHold] = useState('');
+    function handleReveal(){
+        setHold(`${item.title}`);
+    }
     return(
         <>
             <h1>{item.id}</h1>
             <img src={item.image} alt={item.image} />
             <p>{item.title}</p>
+            <button onClick={handleReveal}>click to reveal</button>
+            {hold}
         </>
+        
     )
 }
