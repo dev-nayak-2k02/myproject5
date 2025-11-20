99db500b4b0299454452e81b9498425ed0ce01a8
dev nayak10 <devprasannanayak@gmail.com>
2025-11-20 08:17:08 +0530

    content added

--- 181125reactpracticev1/src/components/Count.jsx
+++ 181125reactpracticev1/src/components/Count.jsx
@@ -1,13 +1,19 @@
 import { useState } from "react";
 export default function Count(){
     const[count,setCount] = useState(0);
-    function handleClick(){
+    const[val,setVal] = useState(1);
+    function handleClick(e){
         setCount(prev=>prev+1);
+        console.log(e.target)
     }
     return(
         <>
             <h1>{count}</h1>
-            <button onClick={handleClick}>Click!</button>
+            <input type="text" value={count} onChange={(e)=>setCount(e.target.value)}/>
+            <span><button onClick={handleClick}>click</button></span>
+            <br />
+            <input type="text" value={val} onChange={(e)=>setVal(e.target.value)}/>
+            <button onClick={()=>setCount(Number(count)+Number(val))}>add</button>
         </>
     )
 }
