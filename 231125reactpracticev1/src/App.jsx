fc271424425e87d62cddf05c9d844eb64b770293
dev nayak10 <devprasannanayak@gmail.com>
2025-11-23 17:30:27 +0530

    content added

--- 231125reactpracticev1/src/App.jsx
+++ 231125reactpracticev1/src/App.jsx
@@ -1,8 +1,10 @@
+import { useState } from "react"
+import Search from "./components/Search"
 function App() {
-
+  const[Item,setItem] = useState([]);
   return (
     <>
-      
+      <Search />
     </>
   )
 }
--- 231125reactpracticev1/src/components/Search.jsx
+++ 231125reactpracticev1/src/components/Search.jsx
@@ -1,1 +1,26 @@
-let URL = 'https://fakestoreapi.com/products?id=integer&title=string&price=number <float>';
+let URL =
+  "https://fakestoreapi.com/products?id=integer&title=string&price=number <float>";
+import React, { useEffect, useState } from "react";
+
+const Search = () => {
+  const [query, setQuery] = useState("");
+  useEffect(() => {
+    async function getQueryItems() {
+        let data = await fetch(`${URL}`);
+        let result = await data.json();
+        
+    }
+    getQueryItems();
+  }, [query]);
+  return (
+    <div>
+      <input
+        type="text"
+        value={query}
+        onChange={(e) => setQuery(e.target.value)}
+      />
+    </div>
+  );
+};
+
+export default Search;
