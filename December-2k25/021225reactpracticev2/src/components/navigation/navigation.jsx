8537b195c0b07ef735512fb66b48107659aa7d5f
dev nayak10 <devprasannanayak@gmail.com>
2025-12-02 20:36:51 +0530

    content added

--- December-2k25/021225reactpracticev2/src/components/navigation/navigation.jsx
+++ December-2k25/021225reactpracticev2/src/components/navigation/navigation.jsx
@@ -1,8 +1,17 @@
 import React from 'react'
-import styles from './Navigation.moule.css'
+import styles from './Navigation.module.css'
 const Navigation = () => {
   return (
-    <div className={`${styles.Navigation} container`}>navigation</div>
+    <nav>
+        <div>
+            <img src="https://images.unsplash.com/photo-1761839258753-85d8eecbbc29?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="" />
+        </div>
+        <ul>
+            <li>Home</li>
+            <li>Contact</li>
+            <li>Product</li>
+        </ul>
+    </nav>
   )
 }
 
