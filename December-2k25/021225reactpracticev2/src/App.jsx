4fbbb192e250a2a2c6758c870fb4da76f5753f13
dev nayak10 <devprasannanayak@gmail.com>
2025-12-03 19:37:01 +0530

    content added

--- December-2k25/021225reactpracticev2/src/App.jsx
+++ December-2k25/021225reactpracticev2/src/App.jsx
@@ -1,10 +1,12 @@
 import ContactHeader from "./components/ConatctHeader/ContactHeader";
+import ContactForm from "./components/ContactForm/ContactForm";
 import Routing from "./Routing";
 function App() {
   return (
     <>
       <Routing />
-      <ContactHeader/>
+      <ContactHeader />
+      <ContactForm />
     </>
   );
 }
--- December-2k25/021225reactpracticev2/src/components/ContactForm/ContactForm.jsx
+++ December-2k25/021225reactpracticev2/src/components/ContactForm/ContactForm.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
+import { MdMessage } from "react-icons/md";
 import styles from './ContactForm.module.css'
+import Buttons from '../Buttons/Buttons'
 const ContactForm = () => {
   return (
     <section className={styles.container}>
-        <div className={styles.contact_form}></div>
+        <div className={styles.contact_form}>
+            <Buttons text='VIA SUPPORT CHAT' icon={<MdMessage/>}/>
+        </div>
         <div className={styles.contact_image}></div>
     </section>
   )
