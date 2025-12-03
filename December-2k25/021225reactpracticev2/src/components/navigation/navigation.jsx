1eadf392eec6eea078583d0621fcb7fd67d09be6
dev nayak10 <devprasannanayak@gmail.com>
2025-12-03 09:55:27 +0530

    content added

--- December-2k25/021225reactpracticev2/src/components/navigation/navigation.jsx
+++ December-2k25/021225reactpracticev2/src/components/navigation/navigation.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import styles from './Navigation.module.css'
+import { Link } from 'react-router-dom'
 const Navigation = () => {
   return (
     <nav className={`${styles.nav_} container`}>
@@ -7,9 +8,9 @@ const Navigation = () => {
             <img src="https://images.unsplash.com/photo-1761839258753-85d8eecbbc29?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="" />
         </div>
         <ul>
-            <li>Home</li>
-            <li>Contact</li>
-            <li>Product</li>
+            <li><Link to='/'>Home</Link></li>
+            <li><Link to='/Contact'>Contact</Link></li>
+            <li><Link to='/Product'>Product</Link></li>
         </ul>
     </nav>
   )
