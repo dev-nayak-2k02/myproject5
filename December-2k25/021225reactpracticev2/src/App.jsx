6bbe2e14f6bef09d3841e52458b3630b888189fe
dev nayak10 <devprasannanayak@gmail.com>
2025-12-03 09:48:43 +0530

    content added

--- December-2k25/021225reactpracticev2/src/App.jsx
+++ December-2k25/021225reactpracticev2/src/App.jsx
@@ -1,10 +1,18 @@
-import Navigation from "./components/navigation/navigation"
+import Navigation from "./components/navigation/navigation";
 function App() {
   return (
     <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/Contact" element={<Contact />} />
+          <Route path="/Product" element={<Product />} />
+          <Route path="*" element={<PageNotFound />} />
+        </Routes>
+      </BrowserRouter>
       <Navigation />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
