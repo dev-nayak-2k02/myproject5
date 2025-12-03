b6de9457e51de0b9e04c0c1112f6be014f8f8be2
dev nayak10 <devprasannanayak@gmail.com>
2025-12-03 10:04:34 +0530

    content added

--- December-2k25/021225reactpracticev2/src/App.jsx
+++ December-2k25/021225reactpracticev2/src/App.jsx
@@ -1,13 +1,18 @@
 import Navigation from "./components/navigation/navigation";
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
+import Home from "./pages/Home";
+import Contact from "./pages/Contact";
+import Product from "./pages/Product";
+import PageNotFound from "./pages/PageNotFound";
 function App() {
   return (
     <>
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/Contact" element={<Contact />} />
-          <Route path="/Product" element={<Product />} />
-          <Route path="*" element={<PageNotFound />} />
+          <Route path="/" element={<Home />}/>
+          <Route path="/Contact" element={<Contact/>}/>
+          <Route path="/Product" element={<Product/>}/>
+          <Route path="/PageNotFound" element={<PageNotFound/>}/>
         </Routes>
       </BrowserRouter>
       <Navigation />
