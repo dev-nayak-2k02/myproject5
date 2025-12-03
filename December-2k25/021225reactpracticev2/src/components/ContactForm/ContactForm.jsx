7f70a7544180dded2ca46717ef3303583dde341e
dev nayak10 <devprasannanayak@gmail.com>
2025-12-03 19:59:12 +0530

    content added

--- December-2k25/021225reactpracticev2/src/components/ContactForm/ContactForm.jsx
+++ December-2k25/021225reactpracticev2/src/components/ContactForm/ContactForm.jsx
@@ -1,18 +1,23 @@
-import React from 'react'
+import React from "react";
 import { MdMessage } from "react-icons/md";
 import { MdCall } from "react-icons/md";
-import styles from './ContactForm.module.css'
-import Buttons from '../Buttons/Buttons'
+import styles from "./ContactForm.module.css";
+import Buttons from "../Buttons/Buttons";
 const ContactForm = () => {
   return (
     <section className={styles.container}>
-        <div className={styles.contact_form}>
-            <Buttons text='VIA SUPPORT CHAT' icon={<MdMessage fontSize='24px'/>}/>
-            <Buttons text='VIA EMAIL CHAT' icon={<MdCall fontSize='24px'/>}/>
+      <div className={styles.contact_form}>
+        <div className={styles.top_btn}>
+          <Buttons
+            text="VIA SUPPORT CHAT"
+            icon={<MdMessage fontSize="24px" />}
+          />
+          <Buttons text="VIA EMAIL CHAT" icon={<MdCall fontSize="24px" />} />
         </div>
-        <div className={styles.contact_image}></div>
+      </div>
+      <div className={styles.contact_image}></div>
     </section>
-  )
-}
+  );
+};
 
-export default ContactForm
+export default ContactForm;
