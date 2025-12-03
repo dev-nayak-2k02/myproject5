8586aab6e6aa481ee4f7764af52f98fa8603d8d9
dev nayak10 <devprasannanayak@gmail.com>
2025-12-03 18:16:04 +0530

    content added

--- December-2k25/021225reactpracticev2/src/components/navigation/navigation.jsx
+++ December-2k25/021225reactpracticev2/src/components/navigation/navigation.jsx
@@ -5,12 +5,12 @@ const Navigation = () => {
   return (
     <nav className={`${styles.nav_} container`}>
         <div className={`${styles.image}`}>
-            <img src="https://images.unsplash.com/photo-1761839258753-85d8eecbbc29?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="" />
+            <img src="/images/logo.png" alt="" />
         </div>
         <ul>
-            <li><Link to='/'>Home</Link></li>
-            <li><Link to='/Contact'>Contact</Link></li>
-            <li><Link to='/Product'>Product</Link></li>
+            <li><Link className={`${styles.linkStyle}`} to='/'>Home</Link></li>
+            <li><Link className={`${styles.linkStyle}`} to='/Contact'>Contact</Link></li>
+            <li><Link className={`${styles.linkStyle}`} to='/Product'>Product</Link></li>
         </ul>
     </nav>
   )
