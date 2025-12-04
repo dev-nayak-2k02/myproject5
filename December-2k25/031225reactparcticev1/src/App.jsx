24571efd8719c8e4aec1e0ade69a14b4973aa202
dev nayak10 <devprasannanayak@gmail.com>
2025-12-04 17:43:51 +0530

    content added

--- December-2k25/031225reactparcticev1/src/App.jsx
+++ December-2k25/031225reactparcticev1/src/App.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Navigation from './components/Navigation/Navigation'
 import ContactHeader from './components/ContactHeader/ContactHeader'
+import ContactForm from './components/ContactForm/ContactForm'
 
 const App = () => {
   return (
     <>
       <Navigation />
       <ContactHeader />
+      <ContactForm />
     </>
   )
 }
---
+++ December-2k25/031225reactparcticev1/src/components/ContactForm/ContactForm.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+const ContactForm = () => {
+  return (
+    <section className={styles.container}>
+        <div className={styles.contact_form}>
+            
+        </div>
+        <div className={styles.contact_image}></div>
+    </section>
+  )
+}
+
+export default ContactForm
