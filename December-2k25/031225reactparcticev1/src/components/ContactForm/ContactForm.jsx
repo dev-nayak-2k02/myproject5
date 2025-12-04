94364dd465ec1d6102337aef1f9ab6e0f91d1417
dev nayak10 <devprasannanayak@gmail.com>
2025-12-04 18:55:12 +0530

    content added

--- December-2k25/031225reactparcticev1/src/components/ContactForm/ContactForm.jsx
+++ December-2k25/031225reactparcticev1/src/components/ContactForm/ContactForm.jsx
@@ -1,20 +1,34 @@
-import React from 'react'
+import React from "react";
 import { MdMessage } from "react-icons/md";
 import { MdCall } from "react-icons/md";
-import styles from './ContactForm.module.css'
-import Button from '../Button/Button'
+import { HiMail } from "react-icons/hi";
+import styles from "./ContactForm.module.css";
+import Button from "../Button/Button";
 const ContactForm = () => {
   return (
     <section className={styles.container}>
-        <div className={styles.contact_form}>
-            <div className={styles.top_btn}>
-                <Button text = "VIA MESSAGE" icon = {<MdMessage fontSize='24px'/>}/>
-                <Button text = "VIA CALL" icon = {<MdCall fontSize='24px'/>}/>
-            </div>
+      <div className={styles.contact_form}>
+        <div className={styles.top_btn}>
+          <Button text="VIA MESSAGE" icon={<MdMessage fontSize="24px" />} />
+          <Button text="VIA CALL" icon={<MdCall fontSize="24px" />} />
         </div>
-        <div className={styles.contact_image}></div>
-    </section>
-  )
-}
 
-export default ContactForm
+        <Button
+          isOutline={true}
+          text="VIA EMAIL FORM"
+          icon={<HiMail fontSize="24px" />}
+        />
+
+        <form >
+            <div className={styles.form_control}>
+                <label htmlFor="name">Name</label>
+                <input type="text" name="name"/>
+            </div>
+        </form>
+      </div>
+      <div className={styles.contact_image}></div>
+    </section>
+  );
+};
+
+export default ContactForm;
