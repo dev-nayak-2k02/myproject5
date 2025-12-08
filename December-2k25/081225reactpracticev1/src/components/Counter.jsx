e53eea227c2af2f0c3c4bf5a1116f0ab6cb71f30
dev nayak10 <devprasannanayak@gmail.com>
2025-12-08 08:56:57 +0530

    content added

--- December-2k25/081225reactpracticev1/src/components/Counter.jsx
+++ December-2k25/081225reactpracticev1/src/components/Counter.jsx
@@ -2,12 +2,13 @@ import React, { useState } from 'react'
 const Counter = () => {
     const[count,setCount] = useState(0)
     function countHandler (){
-        setCount(prev=>prev++);
+        setCount(prev=>prev+1);
     }
   return (
     <div>
-        <button onClick={countHandler}>click me</button>
-        {count}
+        <input type="number" value={count} onChange={(e)=>setCount(e.target.value)}/>
+        <button onClick={countHandler}>click me</button><br /><br />
+        <h2>{count}</h2>
     </div>
   )
 }
