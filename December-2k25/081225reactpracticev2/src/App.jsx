1af4daf8a0d2d8fc0e3c4fbe9b2713960184e452
dev nayak10 <devprasannanayak@gmail.com>
2025-12-08 17:44:49 +0530

    content added

--- December-2k25/081225reactpracticev2/src/App.jsx
+++ December-2k25/081225reactpracticev2/src/App.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import Navigation from './components/Navigation/Navigation'
 
 const App = () => {
   return (
     <div>
-      
+      <Navigation />
     </div>
   )
 }
--- December-2k25/081225reactpracticev2/src/components/Navigation/Navigation.jsx
+++ December-2k25/081225reactpracticev2/src/components/Navigation/Navigation.jsx
@@ -3,9 +3,9 @@ import styles from './Navigation.module.css'
 const Navigation = () => {
   return (
     <div>
-        <nav>
-            <div>
-                <img src="" alt="" />
+        <nav className={`${styles.navigation} container`}>
+            <div className={styles.image}>
+                <img src="/images/logo.png" alt="_pictures_are_subjected_to_market_risk_" />
             </div>
             <ul>
                 <li>Home</li>
