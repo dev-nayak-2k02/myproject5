cb9a4598dfd991e7d03cadcd08f8e9a89ce59d38
dev nayak10 <devprasannanayak@gmail.com>
2025-12-08 18:30:57 +0530

    content added

--- December-2k25/081225reactpracticev2/src/components/ContactForm/ContactForm.jsx
+++ December-2k25/081225reactpracticev2/src/components/ContactForm/ContactForm.jsx
@@ -1,26 +1,50 @@
-import React from 'react'
+import React from "react";
 import { MdMessage } from "react-icons/md";
 import { MdCall } from "react-icons/md";
-import styles from './ContactForm.module.css';
-import Button from '../Buttons/Button';
+import styles from "./ContactForm.module.css";
+import Button from "../Buttons/Button";
 const ContactForm = () => {
   return (
     <section className={styles.container}>
-        <div className={styles.contact_form}>
-            <div className={styles.top_btn}>
-                <Button text='VIA MAIL CHAT' icon={<MdMessage fontSize='24px'/>}/>
-                <Button text='VIA CALL CHAT' icon={<MdCall fontSize='24px'/>}/>
-            </div>
-
-            <Button 
-                isOutline = {true}
-                text='VIA MESSAGE'
-                icon={<MdMessage fontSize='24px'/>}
-            />
+      <div className={styles.contact_form}>
+        <div className={styles.top_btn}>
+          <Button text="VIA MAIL CHAT" icon={<MdMessage fontSize="24px" />} />
+          <Button text="VIA CALL CHAT" icon={<MdCall fontSize="24px" />} />
         </div>
-        <div className={styles.ContactForm}></div>
-    </section>
-  )
-}
 
-export default ContactForm
+        <Button
+          isOutline={true}
+          text="VIA MESSAGE"
+          icon={<MdMessage fontSize="24px" />}
+        />
+
+        <form>
+          <div className={styles.form_control}>
+            <label htmlFor="name">Name</label>
+            <input type="text" name="name" />
+          </div>
+
+          <div className={styles.form_control}>
+            <label htmlFor="mail">email</label>
+            <input type="email" name="mail" />
+          </div>
+
+          <div className={styles.form_control}>
+            <label htmlFor="text">message</label>
+            <textarea rows="8" name="text" />
+          </div>
+        </form>
+        <div style={{
+            display: 'flex',
+            justifyContent: 'space-between'
+        }}>
+            <Button text='cancel'/>
+            <Button text='submit'/>
+        </div>
+      </div>
+      <div className={styles.contact_image}></div>
+    </section>
+  );
+};
+
+export default ContactForm;
