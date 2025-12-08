04be09be3c82c904738180bd60b68167ab67af83
dev nayak10 <devprasannanayak@gmail.com>
2025-12-08 17:40:51 +0530

    content added

---
+++ December-2k25/081225reactpracticev2/src/components/Navigation/Navigation.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import styles from './Navigation.module.css'
+const Navigation = () => {
+  return (
+    <div>
+        <nav>
+            <div>
+                <img src="" alt="" />
+            </div>
+            <ul>
+                <li>Home</li>
+                <li>Contact</li>
+                <li>Product</li>
+            </ul>
+        </nav>
+    </div>
+  )
+}
+
+export default Navigation
