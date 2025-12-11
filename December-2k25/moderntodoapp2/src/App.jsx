200ce25b5e1b3cb633fb77bd19f0e6b969720be1
dev nayak10 <devprasannanayak@gmail.com>
2025-12-11 19:12:32 +0530

    content added

--- December-2k25/moderntodoapp2/src/App.jsx
+++ December-2k25/moderntodoapp2/src/App.jsx
@@ -2,31 +2,40 @@ import React, { useState } from "react";
 import "./App.css";
 const App = () => {
   let [todo, setTodo] = useState([]);
-  let handleSubmit = (e)=>{
+  let list = todo.map((value, index) => {
+    return <TodoItemList key={index} value={value} />;
+  });
+  let handleSubmit = (e) => {
     e.preventDefault();
     let toname = e.target.toname.value;
-    if(!todo.includes(toname)){
-      let finalDoList = [...todo,toname];
+    if (!todo.includes(toname)) {
+      let finalDoList = [...todo, toname];
       setTodo(finalDoList);
     } else {
-      alert('same thing cant be added twice.....');
+      alert("same thing cant be added twice.....");
     }
-  }
+  };
   return (
     <div className="App">
       <h1>Todo-list</h1>
       <form onSubmit={handleSubmit}>
-        <input type="text" name="toname"/>
+        <input type="text" name="toname" />
         <button type="submit">save</button>
       </form>
 
       <div className="outerDiv">
-        <ul>
-          <li>Html <span>&times;</span></li>
-        </ul>
+        <ul>{list}</ul>
       </div>
     </div>
   );
 };
 
 export default App;
+
+function TodoItemList({ value }) {
+  return (
+    <li>
+      {value} <span>&times;</span>
+    </li>
+  );
+}
