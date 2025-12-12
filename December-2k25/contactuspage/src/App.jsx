33426ef2f52f9e2c8aa11224538fd8a61a477558
dev nayak10 <devprasannanayak@gmail.com>
2025-12-12 17:03:41 +0530

    content added

--- December-2k25/contactuspage/src/App.jsx
+++ December-2k25/contactuspage/src/App.jsx
@@ -1,8 +1,13 @@
 import React from 'react'
+import Navigation from './components/Navigation/Navigation'
+import ContactHeader from './components/ContactHeader/ContactHeader'
 
 const App = () => {
   return (
-    <div>App</div>
+    <div>
+      <Navigation />
+      <ContactHeader />
+    </div>
   )
 }
 
