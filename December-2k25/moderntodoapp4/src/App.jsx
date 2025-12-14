8dcde86a0a40b8d1f4ccb1f9c71cdae500cbc4d5
dev nayak10 <devprasannanayak@gmail.com>
2025-12-14 21:55:38 +0530

    content added

---
+++ December-2k25/moderntodoapp4/src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import './App.css'
+const App = () => {
+  return (
+    <div className='App'>
+      <h1>Todo-List</h1>
+      <form action="">
+        <input type="text" />
+        <button>save</button>
+      </form>
+    </div>
+  )
+}
+
+export default App
