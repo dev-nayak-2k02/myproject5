0cdfdca501db0275ce48516fa2243c74de9d9868
dev nayak10 <devprasannanayak@gmail.com>
2025-12-14 22:05:53 +0530

    content added

--- December-2k25/moderntodoapp4/src/App.jsx
+++ December-2k25/moderntodoapp4/src/App.jsx
@@ -1,12 +1,18 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './App.css'
 const App = () => {
+  let [message, setMessage] = useState([]);
+  function handleSubmit(e) {
+      e.preventDefault();
+      let toname = e.target.toname.value;
+      console.log(toname);
+  }
   return (
     <div className='App'>
       <h1>Todo-List</h1>
-      <form action="">
+      <form onSubmit={handleSubmit}>
         <input type="text" />
-        <button>save</button>
+        <button type='submit' name='toname'>save</button>
       </form>
     </div>
   )
