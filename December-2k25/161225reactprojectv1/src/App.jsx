549a0ae12166fdc298ffe2ea26396fa81d1bcf89
dev nayak10 <devprasannanayak@gmail.com>
2025-12-16 10:09:38 +0530

    content added

--- December-2k25/161225reactprojectv1/src/App.jsx
+++ December-2k25/161225reactprojectv1/src/App.jsx
@@ -1,11 +1,17 @@
-import React from 'react'
+import React, { useState } from 'react'
 import styled from 'styled-components'
 import StartGame from './components/StartGame'
 
 const App = () => {
+  const[isGamePlayStarted, setIsGamePlayStarted] = useState(false)
+  let toggleStartGame = () => {
+    setIsGamePlayStarted(prev => !prev)
+  }
   return (
     <div>
-      <StartGame/>
+      {
+        isGamePlayStarted ? "<StartGame />" : "<GamePlay />" 
+      }
     </div>
   )
 }
