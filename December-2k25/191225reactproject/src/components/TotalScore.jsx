413f01ba53e8febf5267deee921e7ac86f804b27
dev nayak10 <devprasannanayak@gmail.com>
2025-12-19 22:13:52 +0530

    content added

--- December-2k25/191225reactproject/src/components/TotalScore.jsx
+++ December-2k25/191225reactproject/src/components/TotalScore.jsx
@@ -1,22 +1,25 @@
-import React from 'react'
-import styled from 'styled-components'
+import React from "react";
+import styled from "styled-components";
 const TotalScore = () => {
   return (
     <ScoreContainer>
-        <h1>0</h1>
-        <p>Total Score</p>
+      <h1>0</h1>
+      <p>Total Score</p>
     </ScoreContainer>
-  )
-}
+  );
+};
 
-export default TotalScore
+export default TotalScore;
 
 const ScoreContainer = styled.div`
-    h1 {
-        font-size: 100px;
-    }
-    p {
-        font-size: 24px;
-        font-weight: 500;
-    }
-`
+  max-width: 200px;
+  text-align: center;
+  h1 {
+    font-size: 100px;
+    line-height: 100px;
+  }
+  p {
+    font-size: 24px;
+    font-weight: 500;
+  }
+`;
