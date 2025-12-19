3d308ca8fea11fc4b3781abf7e32997dbeebd376
dev nayak10 <devprasannanayak@gmail.com>
2025-12-19 21:16:25 +0530

    content added

--- December-2k25/191225reactproject/src/components/StartGame.jsx
+++ December-2k25/191225reactproject/src/components/StartGame.jsx
@@ -1,9 +1,29 @@
 import React from 'react'
-
+import styled from 'styled-components'
 const StartGame = () => {
   return (
-    <div>StartGame</div>
+    <div>
+        <div>
+            <img src="/images/dices.png" alt="___" />
+        </div>
+        <div>
+            <h1>Dice Game</h1>
+            <Button>Click me</Button>
+        </div>
+    </div>
   )
 }
 
 export default StartGame
+
+const Container = styled.div`
+    max-width: 1180px;
+`
+
+const Button = styled.button`
+    width: 220px;
+    padding: 10px 18px;
+    border-radius: 5px;
+    color: white;
+    background-color: #000000;
+`
