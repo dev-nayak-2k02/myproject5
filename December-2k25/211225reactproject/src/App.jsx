0c2980ea5c9a1ccfa11bbb1d7cf0414c9175de94
dev nayak10 <devprasannanayak@gmail.com>
2025-12-21 23:02:41 +0530

    content added

--- December-2k25/211225reactproject/src/App.jsx
+++ December-2k25/211225reactproject/src/App.jsx
@@ -1,16 +1,16 @@
-import React, { useState } from 'react'
-import styled from 'styled-components'
-import StartGame from './components/StartGame'
+import React, { useState } from "react";
+import styled from "styled-components";
+import StartGame from "./components/StartGame";
 const App = () => {
-  let[isGameStarted,setIsGameStarted] = useState(false)
-    const toggleGamePlay = () => {
-        setIsGameStarted(prev => !isGameStarted)
-    }
+  let [isGameStarted, setIsGameStarted] = useState(false);
+  const toggleGamePlay = () => {
+    setIsGameStarted((prev) => !isGameStarted);
+  };
   return (
     <div>
-      {isGameStarted ? <GamePlay/> : <StartGame isToggle={toggleGamePlay}/>}
+      {isGameStarted ? <GamePlay /> : <StartGame isToggle={toggleGamePlay} />}
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
--- December-2k25/211225reactproject/src/components/StartGame.jsx
+++ December-2k25/211225reactproject/src/components/StartGame.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import styled from "styled-components";
-const StartGame = () => {
+const StartGame = ({ isToggle }) => {
   return (
     <Container>
       <div>
@@ -8,11 +8,13 @@ const StartGame = () => {
       </div>
       <div className="content">
         <h1>DICE GAME</h1>
-        <div style={{
-            display: 'flex',
-            justifyContent: 'end'
-        }}>
-            <Button>click me</Button>
+        <div
+          style={{
+            display: "flex",
+            justifyContent: "end",
+          }}
+        >
+          <Button onClick={isToggle}>click me</Button>
         </div>
       </div>
     </Container>
@@ -22,35 +24,35 @@ const StartGame = () => {
 export default StartGame;
 
 const Container = styled.div`
-    max-width: 1180px;
-    margin: 0 auto;
-    display: flex;
-    align-items: center;
-    height: 100vh;
+  max-width: 1180px;
+  margin: 0 auto;
+  display: flex;
+  align-items: center;
+  height: 100vh;
 
-    .content {
-        h1 {
-            font-size: 96px;
-            white-space: nowrap;
-        }
+  .content {
+    h1 {
+      font-size: 96px;
+      white-space: nowrap;
     }
-`
+  }
+`;
 
 const Button = styled.button`
-    width: 220px;
-    font-size: 16px;
-    padding: 10px 18px;
-    background-color: black;
-    color: white;
-    border-radius: 5px;
-    border: 1px solid transparent;
-    transition: 0.4s background ease;
+  width: 220px;
+  font-size: 16px;
+  padding: 10px 18px;
+  background-color: black;
+  color: white;
+  border-radius: 5px;
+  border: 1px solid transparent;
+  transition: 0.4s background ease;
 
-    &:hover {
-        background-color: white;
-        color: black;
-        border: 1px solid black;
-        transition: 0.3s background ease;
-        cursor: pointer;
-    }
-`
+  &:hover {
+    background-color: white;
+    color: black;
+    border: 1px solid black;
+    transition: 0.3s background ease;
+    cursor: pointer;
+  }
+`;
