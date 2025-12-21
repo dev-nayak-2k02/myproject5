3e6380ff7ca99960df912ca2657633e16ee9ca0d
dev nayak10 <devprasannanayak@gmail.com>
2025-12-21 22:55:04 +0530

    content added

--- December-2k25/211225reactproject/src/App.jsx
+++ December-2k25/211225reactproject/src/App.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
-
+import styled from 'styled-components'
+import StartGame from './components/StartGame'
 const App = () => {
   return (
-    <div>App</div>
+    <div>
+      <StartGame/>
+    </div>
   )
 }
 
