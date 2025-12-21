8ec6c20de33a78124ae2e22fafc9838c0ee398aa
dev nayak10 <devprasannanayak@gmail.com>
2025-12-21 23:01:15 +0530

    content added

--- December-2k25/211225reactproject/src/App.jsx
+++ December-2k25/211225reactproject/src/App.jsx
@@ -1,10 +1,14 @@
-import React from 'react'
+import React, { useState } from 'react'
 import styled from 'styled-components'
 import StartGame from './components/StartGame'
 const App = () => {
+  let[isGameStarted,setIsGameStarted] = useState(false)
+    const toggleGamePlay = () => {
+        setIsGameStarted(prev => !isGameStarted)
+    }
   return (
     <div>
-      <StartGame/>
+      {isGameStarted ? <GamePlay/> : <StartGame isToggle={toggleGamePlay}/>}
     </div>
   )
 }
---
+++ December-2k25/211225reactproject/src/components/GamePlay.jsx
@@ -0,0 +1,12 @@
+import React, { useState } from 'react'
+import StartGame from './StartGame'
+const GamePlay = () => {
+    
+  return (
+    <div>
+        
+    </div>
+  )
+}
+
+export default GamePlay
