b47585191528a1ec51d6fd88254b6418527a008a
dev nayak10 <devprasannanayak@gmail.com>
2025-12-22 18:20:44 +0530

    content added

--- December-2k25/221225reactproject/src/components/TotalScore.jsx
+++ December-2k25/221225reactproject/src/components/TotalScore.jsx
@@ -14,4 +14,12 @@ export default TotalScore
 const ScoreContainer = styled.div`
     max-width: 200px;
     text-align: center;
+    h1 {
+        font-size: 100px;
+        line-height: 100px;
+    }
+    p {
+        font-size: 24px;
+        font-weight: 500;
+    }
 `
