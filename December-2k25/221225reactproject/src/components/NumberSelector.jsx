654e211402b27e06e5c14e05c23ed206354bf1a3
dev nayak10 <devprasannanayak@gmail.com>
2025-12-22 18:34:27 +0530

    content added

--- December-2k25/221225reactproject/src/components/NumberSelector.jsx
+++ December-2k25/221225reactproject/src/components/NumberSelector.jsx
@@ -1,24 +1,33 @@
-import React, { useState } from 'react'
-import styled from 'styled-components'
+import React, { useState } from "react";
+import styled from "styled-components";
 const NumberSelector = () => {
-    let arrNumber = [1, 2, 3, 4, 5, 6];
-    let[selectedNumber,setSelectedNumber] = useState()
+  let arrNumber = [1, 2, 3, 4, 5, 6];
+  let [selectedNumber, setSelectedNumber] = useState();
   return (
     <div>
-        {arrNumber.map((value, index) => (
-            <Box key={index}>{value}</Box>
-        ))}
+      {arrNumber.map((value, index) => (
+        <Box
+          isValue={value == selectedNumber}
+          key={index}
+          onClick={() => setSelectedNumber(value)}
+        >
+          {value}
+        </Box>
+      ))}
     </div>
-  )
-}
+  );
+};
 
-export default NumberSelector
+export default NumberSelector;
 
 const Box = styled.div`
-    border: 1px solid black;
-    font-weight: 700;
-    height: 72px;
-    width: 72px;
-    display: grid;
-    place-items: center;
-`
+  border: 1px solid black;
+  font-weight: 700;
+  height: 72px;
+  width: 72px;
+  display: grid;
+  place-items: center;
+  background-color: ${(props) => props.isValue ? 'black' : 'white'};
+  color: ${(props) => !props.isValue ? 'black' : 'white'};
+  cursor: pointer;
+`;
