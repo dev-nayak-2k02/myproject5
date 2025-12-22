f8ae4407f60ba85e8850ce452a90d630e2607aff
dev nayak10 <devprasannanayak@gmail.com>
2025-12-22 17:49:17 +0530

    content added

---
+++ December-2k25/221225reactproject/src/components/StartGame.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import styled from 'styled-components'
+const StartGame = () => {
+  return (
+    <div>
+        <Button>click me to know!</Button>
+    </div>
+  )
+}
+
+export default StartGame
+
+const Button = styled.button`
+    background-color: black;
+    color: white;
+`
