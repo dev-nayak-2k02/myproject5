aa70394f7156443b93b7cd2eee0d22af7b0af069
dev nayak10 <devprasannanayak@gmail.com>
2025-12-22 18:01:50 +0530

    content added

---
+++ December-2k25/221225reactproject/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import StartGame from './components/StartGame'
+
+const App = () => {
+  return (
+    <div>
+        <StartGame />
+    </div>
+  )
+}
+
+export default App
--- December-2k25/221225reactproject/src/components/StartGame.jsx
+++ December-2k25/221225reactproject/src/components/StartGame.jsx
@@ -2,15 +2,53 @@ import React from 'react'
 import styled from 'styled-components'
 const StartGame = () => {
   return (
-    <div>
-        <Button>click me to know!</Button>
-    </div>
+    <Container>
+        <div>
+            <img src="/images/dices.png" alt="/00/00/" />
+        </div>
+        <div className='content'>
+            <h1>DICE GAME</h1>
+            <div style={{
+                display: 'flex',
+                justifyContent: 'end'
+            }}>
+                <Button>click me</Button>
+            </div>
+        </div>
+    </Container>
   )
 }
 
 export default StartGame
 
+const Container = styled.div`
+    max-width: 1180px;
+    margin: 0 auto;
+    height: 100vh;
+    display: flex;
+    align-items: center;
+
+    .content {
+        h1{
+            font-size: 96px;
+            white-space: nowrap;
+        }
+    }
+`
 const Button = styled.button`
+    width: 220px;
     background-color: black;
     color: white;
+    padding: 10px 18px;
+    border-radius: 5px;
+    font-size: 16px;
+    border: 1px solid transparent;
+    transition: 0.3s background ease;
+
+    &:hover {
+        background-color: white;
+        color: black;
+        border: 1px solid black;
+        transition: 0.3s background ease;
+    }
 `
