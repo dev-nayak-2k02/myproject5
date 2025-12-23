6e33220cf05ffee1b679824455a972dfdb4e3e2d
dev nayak10 <devprasannanayak@gmail.com>
2025-12-23 18:25:08 +0530

    content added

--- December-2k25/231225reactproject/src/components/GamePlay.jsx
+++ December-2k25/231225reactproject/src/components/GamePlay.jsx
@@ -1,14 +1,25 @@
-import React from 'react'
-import TotalScore from './TotalScore'
-import NumberSelector from './NumberSelector'
+import React from "react";
+import TotalScore from "./TotalScore";
+import NumberSelector from "./NumberSelector";
+import styled from "styled-components";
 
 const GamePlay = () => {
   return (
-    <div>
+    <MainContainer>
+      <div className="top_section">
         <TotalScore />
         <NumberSelector />
-    </div>
-  )
-}
+      </div>
+    </MainContainer>
+  );
+};
 
-export default GamePlay
+export default GamePlay;
+
+const MainContainer = styled.main`
+    padding-top: 70px;
+    .top_section {
+        display: flex;
+        justify-content: space-around;
+    }
+`;
--- December-2k25/231225reactproject/src/components/NumberSelector.jsx
+++ December-2k25/231225reactproject/src/components/NumberSelector.jsx
@@ -2,23 +2,45 @@ import React, { useState } from "react";
 import styled from "styled-components";
 const NumberSelector = () => {
   let arrNumber = [1, 2, 3, 4, 5, 6];
-  const [selectedNumber, setSelectedNumber] = useState(1);
+  const [selectedNumber, setSelectedNumber] = useState();
   return (
-    <div>
-      {arrNumber.map((value, index) => (
-        <Box
-          isValue={value == selectedNumber}
-          onClick={() => setSelectedNumber(value)}
-          key={index}
-        >
-          {value}
-        </Box>
-      ))}
-    </div>
+    <NumberSelectorContainer>
+      <div className="flex">
+        {arrNumber.map((value, index) => (
+          <Box
+            isValue={value == selectedNumber}
+            onClick={() => setSelectedNumber(value)}
+            key={index}
+          >
+            {value}
+          </Box>
+        ))}
+      </div>
+      <div style={{display: 'flex', justifyContent: 'end'}}>
+        <p>Select Number</p>
+      </div>
+    </NumberSelectorContainer>
   );
 };
 
 export default NumberSelector;
+
+const NumberSelectorContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: end;
+
+  p {
+    padding-top: 20px;
+    font-size: 24px;
+    font-weight: 700;
+  }
+
+  .flex {
+    display: flex;
+    gap: 24px;
+  }
+`;
 
 const Box = styled.div`
   height: 72px;
@@ -27,6 +49,10 @@ const Box = styled.div`
   font-weight: 700;
   display: grid;
   place-items: center;
-  background-color: ${(props) => props.isValue ? 'black' : 'white'};
-  color: ${(props) => !props.isValue ? 'black' : 'white'};
+  background-color: ${(props) => (props.isValue ? "black" : "white")};
+  color: ${(props) => (!props.isValue ? "black" : "white")};
+
+  &:hover {
+    cursor: pointer;
+  }
 `;
