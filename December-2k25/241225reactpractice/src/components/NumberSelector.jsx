76fbd7a06baee2449caa0a9b80043c2516dfc3e0
dev nayak10 <devprasannanayak@gmail.com>
2025-12-25 15:57:33 +0530

    content added

--- December-2k25/241225reactpractice/src/components/NumberSelector.jsx
+++ December-2k25/241225reactpractice/src/components/NumberSelector.jsx
@@ -4,23 +4,53 @@ const NumberSelector = () => {
   const arrNumber = [1, 2, 3, 4, 5, 6];
   const [isColor, setIsColor] = useState();
   return (
-    <div>
-      {arrNumber.map((value, index) => (
-        <Box
-          isValue={value == isColor}
-          onClick={() => {
-            setIsColor(value);
-          }}
-          key={index}
-        >
-          {value}
-        </Box>
-      ))}
-    </div>
+    <NumberSelectorContainer>
+      <div className="flex">
+        {arrNumber.map((value, index) => (
+          <Box
+            isValue={value == isColor}
+            onClick={() => {
+              setIsColor(value);
+            }}
+            key={index}
+          >
+            {value}
+          </Box>
+        ))}
+      </div>
+      <p>Select Number</p>
+    </NumberSelectorContainer>
   );
 };
 
 export default NumberSelector;
+
+const NumberSelectorContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: end;
+  justify-content: end;
+
+  .error {
+    color: red;
+    font-size: 16px;
+    font-weight: 500;
+
+    span {
+      color: white;
+    }
+  }
+
+  p {
+    font-size: 24px;
+    font-weight: 700;
+  }
+
+  .flex {
+    display: flex;
+    gap: 24px;
+  }
+`;
 
 const Box = styled.div`
   height: 72px;
@@ -29,6 +59,6 @@ const Box = styled.div`
   font-weight: 700;
   display: grid;
   place-items: center;
-  background-color: ${(props) => props.isValue ? 'black' : 'white'};
-  color: ${(props) => !props.isValue ? 'black' : 'white'};
+  background-color: ${(props) => (props.isValue ? "black" : "white")};
+  color: ${(props) => (!props.isValue ? "black" : "white")};
 `;
