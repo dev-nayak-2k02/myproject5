d74c42a5f45e5d718cdc2f4e04ceb2790a093563
dev nayak10 <devprasannanayak@gmail.com>
2025-12-27 20:23:32 +0530

    content added

--- December-2k25/271225reactpractice/src/componets/NumberSelector.jsx
+++ December-2k25/271225reactpractice/src/componets/NumberSelector.jsx
@@ -1,9 +1,26 @@
-import React from 'react'
-
+import React, { useState } from 'react'
+import styled from 'styled-components'
 const NumberSelector = () => {
+  let arrNumber = [1, 2, 3, 4, 5, 6];
+  const[boxSelector, setBoxSelector] = useState()
   return (
-    <div>NumberSelector</div>
+    <div>
+      {
+        arrNumber.map((value, index) =>(
+          <Box>{value}</Box>
+        ))
+      }
+    </div>
   )
 }
 
 export default NumberSelector
+
+const Box = styled.div`
+  height: 72px;
+  width: 72px;
+  border: 1px solid black;
+  font-weight: 700;
+  display: grid;
+  place-items: center;
+`
