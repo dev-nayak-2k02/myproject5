ae68f22055ecb4e3daafb650b4618045450318f2
dev nayak10 <devprasannanayak@gmail.com>
2025-12-30 21:36:25 +0530

    content added

--- December-2k25/301225reactpractice/src/components/NumberSelector.jsx
+++ December-2k25/301225reactpractice/src/components/NumberSelector.jsx
@@ -1,9 +1,14 @@
 import React from 'react'
 import styled from 'styled-components'
 const NumberSelector = () => {
+    const arrNumber = [1, 2, 3, 4, 5, 6];
   return (
     <div>
-        <Box>1</Box>
+        {
+            arrNumber.map((value, index) => {
+                <Box key={index}>{value}</Box>
+            })
+        }
     </div>
   )
 }
@@ -11,5 +16,9 @@ const NumberSelector = () => {
 export default NumberSelector
 
 const Box = styled.div`
-    
+    height: 72px;
+    width: 72px;
+    border: 1px solid black;
+    display: grid;
+    place-items: center;
 `
