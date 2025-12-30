ddc6a84ecf4118179beba1623ec601c3e00b28c6
dev nayak10 <devprasannanayak@gmail.com>
2025-12-30 19:58:50 +0530

    content added

---
+++ December-2k25/301225reactpractice/src/components/StartGame.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import styled from 'styled-components'
+const StartGame = () => {
+  return (
+    <MainContainer>
+        
+    </MainContainer>
+  )
+}
+
+export default StartGame
+
+const MainContainer = styled.div`
+    max-width: 1180px;
+    background-color: red;
+    height: 100vh;
+    margin: 0 auto;
+    
+`
+
+const Button = styled.button`
+    background-color: black;
+    color: white;
+    padding: 10px 18px;
+    width: 220px;
+    border-radius: 5px;
+`
