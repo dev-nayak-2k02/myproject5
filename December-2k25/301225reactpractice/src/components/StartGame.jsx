0aefadebd722e38ce069c326cb6885dc8d6ae19e
dev nayak10 <devprasannanayak@gmail.com>
2025-12-30 20:20:40 +0530

    content added

--- December-2k25/301225reactpractice/src/components/StartGame.jsx
+++ December-2k25/301225reactpractice/src/components/StartGame.jsx
@@ -44,6 +44,14 @@ const Button = styled.button`
     width: 220px;
     border-radius: 5px;
     font-size: 16px;
+    border: 1px solid transparent;
+    transition: 0.4s background ease;
 
-    
+    &:hover {
+        background-color: white;
+        color: black;
+        border: 1px solid black;
+        transition: 0.3s background ease;
+        cursor: pointer;
+    }
 `
