a499b86dbde6002185abfc37f17dd64ff1f6613e
dev nayak10 <devprasannanayak@gmail.com>
2025-12-30 20:27:14 +0530

    content added

--- December-2k25/301225reactpractice/src/App.jsx
+++ December-2k25/301225reactpractice/src/App.jsx
@@ -1,11 +1,16 @@
 import React, { useState } from 'react'
 import StartGame from './components/StartGame'
+import GameStarted from './components/GameStarted'
 const App = () => {
   const [isGameStarted, setIsGameStarted] = useState(false)
+
+  const toggleFunction = () =>{
+    setIsGameStarted(prev => !isGameStarted)
+  }
   return (
     <div>
       {
-        isGameStarted 
+        isGameStarted ? <GameStarted/> : <StartGame isToggle={toggleFunction}/>
       }
     </div>
   )
--- December-2k25/301225reactpractice/src/components/StartGame.jsx
+++ December-2k25/301225reactpractice/src/components/StartGame.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import styled from 'styled-components'
-const StartGame = () => {
+const StartGame = ({ isToggle }) => {
   return (
     <Container>
         <div>
@@ -9,7 +9,7 @@ const StartGame = () => {
         <div className='content'>
             <h1>DICE GAME</h1>
             <div className='content-btn'>
-                <Button>click me</Button>
+                <Button onClick={isToggle}>click me</Button>
             </div>
         </div>
     </Container>
