3d1a238074abaa0814a4404fea5525a2c102451a
dev nayak10 <devprasannanayak@gmail.com>
2026-01-01 22:04:34 +0530

    content added

--- 2k26/january - 2k26/010126reactpractice/src/components/GameButtons.jsx
+++ 2k26/january - 2k26/010126reactpractice/src/components/GameButtons.jsx
@@ -1,11 +1,11 @@
-import React from 'react'
+import React, { useState } from 'react'
 import styled from 'styled-components'
 const GameButtons = () => {
-
+    
   return (
-    <div>
-
-    </div>
+    <ButtonContainer>
+        <Button>reset</Button>
+    </ButtonContainer>
   )
 }
 
@@ -15,4 +15,23 @@ const ButtonContainer = styled.div`
     display: flex;
     flex-direction: column;
     align-items: center;
+    margin-top: 10px;
+`
+
+const Button = styled.button`
+  background-color: black;
+  color: white;
+  padding: 10px 18px;
+  width: 150px;
+  border-radius: 5px;
+  border: 1px solid transparent;
+  transition: 0.4s background ease;
+  cursor: pointer;
+
+  &:hover {
+    background-color: white;
+    color: black;
+    border: 1px solid black;
+    transition: 0.3s background ease;
+  }
 `
--- 2k26/january - 2k26/010126reactpractice/src/components/GamePlay.jsx
+++ 2k26/january - 2k26/010126reactpractice/src/components/GamePlay.jsx
@@ -7,29 +7,29 @@ import GameButtons from "./GameButtons";
 const GamePlay = () => {
   const [rollDice, setRollDice] = useState(1);
   const [numberSelector, setNumberSelector] = useState();
-  const [score, setScore] = useState(0)
-  const [error, setError] = useState('')
+  const [score, setScore] = useState(0);
+  const [error, setError] = useState("");
 
   const generteRandomNumber = (min, max) => {
     return Math.floor(Math.random() * (max - min) + min);
   };
 
   const diceRoll = () => {
-    if(!numberSelector){
-      setError('select a number first')
-      return
+    if (!numberSelector) {
+      setError("select a number first");
+      return;
     }
 
     let random = generteRandomNumber(1, 7);
 
-    if(random == numberSelector) {
-      setScore(prev => prev + random)
+    if (random == numberSelector) {
+      setScore((prev) => prev + random);
     } else {
-      setScore(prev => prev - 2)
+      setScore((prev) => prev - 2);
     }
 
     setRollDice((prev) => random);
-    setNumberSelector(undefined)
+    setNumberSelector(undefined);
   };
 
   return (
@@ -44,7 +44,12 @@ const GamePlay = () => {
         />
       </div>
       <RollDice rollDice={rollDice} diceRoll={diceRoll} />
-      <GameButtons />
+      <GameButtons
+        setScore={setScore}
+        setError={setError}
+        setNumberSelector={setNumberSelector}
+        setRollDice={setRollDice}
+      />
     </MainContainer>
   );
 };
