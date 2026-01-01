0370a88c74ea952154dad0272c672a57f71c8fda
dev nayak10 <devprasannanayak@gmail.com>
2026-01-01 10:56:16 +0530

    content added

--- 2k26/january - 2k26/010126reactpractice/src/components/GamePlay.jsx
+++ 2k26/january - 2k26/010126reactpractice/src/components/GamePlay.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
 import styled from 'styled-components'
+import TotalScore from './TotalScore'
 const GamePlay = () => {
   return (
     <div>
-        
+        <TotalScore/>
     </div>
   )
 }
--- 2k26/january - 2k26/010126reactpractice/src/components/TotalScore.jsx
+++ 2k26/january - 2k26/010126reactpractice/src/components/TotalScore.jsx
@@ -1,9 +1,30 @@
 import React from 'react'
-
+import styled from 'styled-components'
 const TotalScore = () => {
   return (
-    <div>TotalScore</div>
+    <StoreContainer>
+        <h1>0</h1>
+        <p>Total Score</p>
+    </StoreContainer>
   )
 }
 
 export default TotalScore
+
+const StoreContainer = styled.div`
+    width: 200px;
+    background-color: red;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+
+    h1{
+        font-size: 100px;
+        line-height: 100px;
+    }
+
+    p{
+        font-size: 24px;
+        font-weight: 500;
+    }
+`
