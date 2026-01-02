d883a604a11d16b9963c012a505af4925febadc7
dev nayak10 <devprasannanayak@gmail.com>
2026-01-02 19:58:33 +0530

    content added

--- 2k26/january - 2k26/020126reactpractice/src/components/TotalScore.jsx
+++ 2k26/january - 2k26/020126reactpractice/src/components/TotalScore.jsx
@@ -1,9 +1,28 @@
 import React from 'react'
-
+import styled from 'styled-components'
 const TotalScore = () => {
   return (
-    <div>TotalScore</div>
+    <ScoreContainer>
+        <h1>0</h1>
+        <p>Total Score</p>
+    </ScoreContainer>
   )
 }
 
 export default TotalScore
+
+const ScoreContainer = styled.div`
+    width: 200px;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+
+    h1 {
+        font-size: 100px;
+        line-height: 100px;
+    }
+    p {
+        font-size: 24px;
+        font-weight: 500;
+    }
+`
