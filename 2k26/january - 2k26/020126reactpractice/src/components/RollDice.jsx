1cec18a27373401d570835401350ccd6e6be97c5
dev nayak10 <devprasannanayak@gmail.com>
2026-01-02 20:46:00 +0530

    content added

--- 2k26/january - 2k26/020126reactpractice/src/components/RollDice.jsx
+++ 2k26/january - 2k26/020126reactpractice/src/components/RollDice.jsx
@@ -1,18 +1,35 @@
-import React from 'react'
-import styled from 'styled-components'
+import React, { useState } from "react";
+import styled from "styled-components";
 const RollDice = () => {
+  const [rollDice, setRollDice] = useState(1);
+  const randomNumberGenerator = (min, max) => {
+    return Math.floor(Math.random() * (max - min) + min);
+  };
+  const handleDiceRoll = () => {
+    let random = randomNumberGenerator(1, 7);
+    setRollDice((prev) => random);
+  };
   return (
     <DiceContainer>
-        <div >
-            <img src="" alt="" />
-        </div>
-        <p>click on dice to roll</p>
+      <div className="dice" onClick={handleDiceRoll}>
+        <img src={`/images/dices/dice_${rollDice}.png`} alt="00-000-000000" />
+      </div>
+      <p>click on dice to roll</p>
     </DiceContainer>
-  )
-}
+  );
+};
 
-export default RollDice
+export default RollDice;
 
 const DiceContainer = styled.div`
-    
-`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin-top: 48px;
+  cursor: pointer;
+
+  p {
+    font-size: 18px;
+    font-weight: 500;
+  }
+`;
