534e997e31bcd1865a366db06593a572c6f593ff
dev nayak10 <devprasannanayak@gmail.com>
2026-01-02 19:35:12 +0530

    content added

---
+++ 2k26/january - 2k26/020126reactpractice/src/components/StartGame.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import styled from 'styled-components'
+const StartGame = () => {
+  return (
+    <Container></Container>
+  )
+}
+
+export default StartGame
+
+const Container = styled.div`
+    width: 200px;
+    height: 100vh;
+    background-color: red;
+`
