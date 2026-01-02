7f353c83a6b9f1f8dd9fa238f7eacaf3ad6f5b57
dev nayak10 <devprasannanayak@gmail.com>
2026-01-02 21:11:26 +0530

    content added

--- 2k26/january - 2k26/020126reactpractice/src/components/GamePlay.jsx
+++ 2k26/january - 2k26/020126reactpractice/src/components/GamePlay.jsx
@@ -8,13 +8,28 @@ const GamePlay = () => {
   const [numberSelect, setNumberSelect] = useState();
   const [rollDice, setRollDice] = useState(1);
   const [score, setScore] = useState(0);
+  const [error, setError] = useState('')
 
   const randomNumberGenerator = (min, max) => {
     return Math.floor(Math.random() * (max - min) + min);
   };
   const handleDiceRoll = () => {
+    if(!numberSelect) {
+        setError('select a number first')
+        return
+    }
+
     let random = randomNumberGenerator(1, 7);
+
+    if(random == numberSelect) {
+        setScore(prev => prev + random)
+    } else {
+        setScore(prev => prev * 0)
+    }
+
     setRollDice((prev) => random);
+
+    setNumberSelect(undefined)
   };
 
   return (
@@ -24,6 +39,8 @@ const GamePlay = () => {
         <NumberSelector
           numberSelect={numberSelect}
           setNumberSelect={setNumberSelect}
+          error={error}
+          setError={setError}
         />
       </div>
       <RollDice rollDice={rollDice} handleDiceRoll={handleDiceRoll} />
--- 2k26/january - 2k26/020126reactpractice/src/components/NumberSelector.jsx
+++ 2k26/january - 2k26/020126reactpractice/src/components/NumberSelector.jsx
@@ -1,14 +1,22 @@
 import React, { useState } from "react";
 import styled from "styled-components";
-const NumberSelector = ({ numberSelect, setNumberSelect }) => {
+const NumberSelector = ({ numberSelect, setNumberSelect, error, setError }) => {
   const arrNumber = [1, 2, 3, 4, 5, 6];
+  const errorHandler = (value) => {
+    setNumberSelect(value);
+    setError("");
+  };
   return (
     <NumberSelectorContainer>
+      <p className="error">
+        <span>''</span>
+        {error}
+      </p>
       <div className="flex">
         {arrNumber.map((value, index) => (
           <Box
             isValue={value == numberSelect}
-            onClick={() => setNumberSelect(value)}
+            onClick={() => errorHandler(value)}
             key={index}
           >
             {value}
@@ -28,6 +36,15 @@ const NumberSelectorContainer = styled.div`
   display: flex;
   flex-direction: column;
   align-items: end;
+
+  .error {
+    color: red;
+    font-size: 16px;
+
+    span {
+      color: white;
+    }
+  }
 
   p {
     font-size: 24px;
