4e7de4aa3989acd5d0cfa8da80f2b983fdb618ab
dev nayak10 <devprasannanayak@gmail.com>
2026-01-02 20:28:16 +0530

    content added

---
+++ 2k26/january - 2k26/020126reactpractice/src/components/RollDice.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import styled from 'styled-components'
+const RollDice = () => {
+  return (
+    <DiceContainer>
+        <div >
+            <img src="" alt="" />
+        </div>
+        <p>click on dice to roll</p>
+    </DiceContainer>
+  )
+}
+
+export default RollDice
+
+const DiceContainer = styled.div`
+    
+`
