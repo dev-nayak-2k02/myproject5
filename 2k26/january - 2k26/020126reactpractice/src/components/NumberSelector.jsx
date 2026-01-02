bb8b17001f6e5a58c67c4be6bac89d1325abf97f
dev nayak10 <devprasannanayak@gmail.com>
2026-01-02 20:01:14 +0530

    content added

--- 2k26/january - 2k26/020126reactpractice/src/components/NumberSelector.jsx
+++ 2k26/january - 2k26/020126reactpractice/src/components/NumberSelector.jsx
@@ -2,8 +2,15 @@ import React from 'react'
 import styled from 'styled-components'
 const NumberSelector = () => {
   return (
-    <div>NumberSelector</div>
+    <div>
+        <Box>1</Box>
+    </div>
   )
 }
 
 export default NumberSelector
+
+const Box = styled.div`
+    width: 72px;
+    height: 72px;
+`
