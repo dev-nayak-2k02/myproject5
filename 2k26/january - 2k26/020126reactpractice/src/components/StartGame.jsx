9fb18f4c08e938bafe05a49384819f5513970c8c
dev nayak10 <devprasannanayak@gmail.com>
2026-01-02 19:45:43 +0530

    content added

--- 2k26/january - 2k26/020126reactpractice/src/components/StartGame.jsx
+++ 2k26/january - 2k26/020126reactpractice/src/components/StartGame.jsx
@@ -3,7 +3,13 @@ import styled from 'styled-components'
 const StartGame = () => {
   return (
     <Container>
-        <div></div>
+        <div>
+            <img src="/images/dices.png" alt="" />
+        </div>
+        <div className='content'>
+            <h1>DICE GAME</h1>
+            <Button>click me</Button>
+        </div>
     </Container>
   )
 }
@@ -14,5 +20,35 @@ const Container = styled.div`
     width: 1180px;
     height: 100vh;
     margin: 0 auto;
-    background-color: red;
+    display: flex;
+    align-items: center;
+
+    .content {
+        display: flex;
+        flex-direction: column;
+        align-items: end;
+
+        h1 {
+            font-size: 96px;
+            white-space: nowrap;
+        }
+    }
+`
+const Button = styled.button`
+    width: 200px;
+    padding: 10px 18px;
+    background-color: black;
+    color: white;
+    border-radius: 5px;
+    font-size: 16px;
+    transition: 0.3s background ease;
+    border: 1px solid transparent;
+    cursor: pointer;
+
+    &:hover{
+        background-color: white;
+        color: black;
+        border: 1px solid black;
+        transition: 0.3s background ease;
+    }
 `
