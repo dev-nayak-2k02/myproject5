1cff83fcaf298d44951ab998d3a91b8402fb7686
dev nayak10 <devprasannanayak@gmail.com>
2026-01-03 15:44:01 +0530

    content added

--- 2k26/january - 2k26/030126reactpractice/src/components/GamePlay.jsx
+++ 2k26/january - 2k26/030126reactpractice/src/components/GamePlay.jsx
@@ -6,29 +6,43 @@ import RollDice from "./RollDice";
 const GamePlay = () => {
   const [rollDice, setRollDice] = useState(1);
   const [numberSelect, setNumberSelect] = useState();
-  const [score, setScore] = useState(0)
-  
+  const [score, setScore] = useState(0);
+  const [error, setError] = useState("");
+
   const randomNumberGenerator = (min, max) => {
     return Math.floor(Math.random() * (max - min) + min);
   };
 
-  const handleDice = ()=>{
-    let random = randomNumberGenerator(1, 7)
-
-    if(random == rollDice){
-      setScore((prev) => prev + random)
-    } else {
-      setScore((prev) => prev - 2)
+  const handleDice = () => {
+    if (!numberSelect) {
+      setError("select a number first");
+      return;
     }
-    setRollDice(prev => random)
-  }
+
+    let random = randomNumberGenerator(1, 7);
+
+    if (random == numberSelect) {
+      setScore((prev) => prev + random);
+    } else {
+      setScore((prev) => prev - 2);
+    }
+
+    setRollDice((prev) => random);
+
+    setNumberSelect(undefined)
+  };
   return (
     <MainContainer>
       <div className="top-section">
-        <TotalScore score={score}/>
-        <NumberSelector numberSelect={numberSelect} setNumberSelect={setNumberSelect}/>
+        <TotalScore score={score} />
+        <NumberSelector
+          numberSelect={numberSelect}
+          setNumberSelect={setNumberSelect}
+          error={error}
+          setError={setError}
+        />
       </div>
-      <RollDice rollDice={rollDice} handleDice={handleDice}/>
+      <RollDice rollDice={rollDice} handleDice={handleDice} />
     </MainContainer>
   );
 };
@@ -41,4 +55,4 @@ const MainContainer = styled.div`
     display: flex;
     justify-content: space-around;
   }
-`
+`;
