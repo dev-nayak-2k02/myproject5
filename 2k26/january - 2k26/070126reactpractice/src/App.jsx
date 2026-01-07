567cf7620c2e7f7a75ea1211a4cd665800bbf536
dev nayak10 <devprasannanayak@gmail.com>
2026-01-07 17:30:02 +0530

    content added

--- 2k26/january - 2k26/070126reactpractice/src/App.jsx
+++ 2k26/january - 2k26/070126reactpractice/src/App.jsx
@@ -1,9 +1,18 @@
 import React from 'react'
-
+import styled from 'styled-components'
 const App = () => {
   return (
-    <div>App</div>
+    <div>
+      <Button></Button>
+    </div>
   )
 }
 
 export default App
+
+const Button = styled.button`
+  background-color: black;
+  color: white;
+  width: 200px;
+  padding: 10px 18px;
+`
