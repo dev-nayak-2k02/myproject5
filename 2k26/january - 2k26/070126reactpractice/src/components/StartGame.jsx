9cb7e9e2e74d4237a689bd93750642276d4b41be
dev nayak10 <devprasannanayak@gmail.com>
2026-01-07 17:41:28 +0530

    content added

--- 2k26/january - 2k26/070126reactpractice/src/components/StartGame.jsx
+++ 2k26/january - 2k26/070126reactpractice/src/components/StartGame.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import styled from 'styled-components'
 
-const StartGame = () => {
+const StartGame = ({isToggle}) => {
   return (
     <Container>
         <div>
@@ -9,7 +9,7 @@ const StartGame = () => {
         </div>
         <div className="content">
             <h1>DICE GAME</h1>
-            <Button>click me</Button>
+            <Button onClick={isToggle}>click me</Button>
         </div>
     </Container>
   )
