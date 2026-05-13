04c51c900a1846c4d7471d63939d698cf0491ac1
dev nayak10 <devprasannanayak@gmail.com>
2026-05-13 09:32:24 +0530

    content added

--- 2k26/may - 2k26/130526reactpractice/src/App.jsx
+++ 2k26/may - 2k26/130526reactpractice/src/App.jsx
@@ -2,11 +2,21 @@ import React from "react";
 import Greetings from "./Greetings";
 import { Welcome } from "./Greetings";
 function App(){
+  let name = "Dev Nayak";
+  let age = 23;
+  let gender = 'male'
+  let isVoting = 1;
   return(
     <>
       <Welcome/>
       <h1>hello there,</h1>
       <p><Greetings/> and I'm revising React to get better at it</p>
+      <ul>
+        <li>name:{name}</li>
+        <li>age:{age}</li>
+        <li>gender:{gender}</li>
+        <li>voting:{Boolean(isVoting).toString()}</li>
+      </ul>
     </>
   )
 }
