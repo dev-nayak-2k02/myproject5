79370697032905cfc7870ff74e4f26348a9ce32a
dev nayak10 <devprasannanayak@gmail.com>
2026-05-14 08:11:15 +0530

    content added

--- 2k26/may - 2k26/140526reactpractice/src/App.jsx
+++ 2k26/may - 2k26/140526reactpractice/src/App.jsx
@@ -1,9 +1,15 @@
 import React from "react"
 import './App.css'
+import { Exampler, Greetings, Welcome } from "./Exampler";
 export default function App(){
   return(
     <div>
       <h1>Dev Prasanna Nayak</h1>
+      <ul>
+        <li><Greetings/></li>
+        <li><Welcome/></li>
+        <li><Exampler/></li>
+      </ul>
     </div>
   )
 }
--- 2k26/may - 2k26/140526reactpractice/src/Exampler.jsx
+++ 2k26/may - 2k26/140526reactpractice/src/Exampler.jsx
@@ -22,3 +22,5 @@ function Welcome(){
         </>
     )
 }
+
+export {Exampler, Greetings, Welcome}
