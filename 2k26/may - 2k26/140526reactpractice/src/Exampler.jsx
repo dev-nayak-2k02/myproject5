c0430a279be0faf3cb059c0489ec9cbcf7a77792
dev nayak10 <devprasannanayak@gmail.com>
2026-05-14 08:06:31 +0530

    content added

---
+++ 2k26/may - 2k26/140526reactpractice/src/Exampler.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+let Exampler = ()=>{
+    return(
+        <>
+            <p>This is exampler function declared in another form</p>
+        </>
+    )
+}
+
+function Greetings(){
+    return(
+        <>
+            <p>Hey there, I'm Dev building a react project.</p>
+        </>
+    )
+}
+
+function Welcome(){
+    return(
+        <>
+            <p>Welcome folks, this is me building a website.</p>
+        </>
+    )
+}
