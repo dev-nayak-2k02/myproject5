8c265b746965dc4fb62bebeca1bb2ad09227b167
dev nayak10 <devprasannanayak@gmail.com>
2026-05-16 09:06:58 +0530

    content added

--- 2k26/may - 2k26/140526reactpractice/src/App.jsx
+++ 2k26/may - 2k26/140526reactpractice/src/App.jsx
@@ -1,30 +1,15 @@
-import React from "react"
+import React, { useState } from "react"
 import './App.css'
 import { Exampler, Greetings, Welcome } from "./Exampler";
-import Student from "./Student";
-export default function App(){
-  let name = 'Dev Nayak';
-  let age = 23;
-  let gender = 'Male';
-  let isVoting = 'Eligible'
-  function clickMessage(){
-    console.log('Clicked');
-  }
+function App(){
+  const[count, setCount] = useState(0);
   return(
-    <div>
-      <h1>Dev Prasanna Nayak</h1>
-      <ul>
-        <li className="list-items"><Greetings/></li>
-        <li className="list-items"><Welcome/></li>
-        <li className="list-items"><Exampler/></li>
-        <li>{name}</li>
-        <li>{age}</li>
-        <li>{gender}</li>
-        <li>{isVoting}</li>
-        <li><Student name={name} age={age} message={clickMessage}/></li>
-        
-      </ul>
-      
-    </div>
+    <>
+      <Exampler/>
+      <p><b>counter App</b></p>
+      <button onClick={setCount((prev)=>prev + 1)}></button>
+      <p>{count}</p>
+    </>
   )
 }
+export default App;
