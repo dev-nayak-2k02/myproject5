d328f7a5fd591c4fd89ffca28b68edc2ae7dba31
dev nayak10 <devprasannanayak@gmail.com>
2026-07-20 08:41:46 +0530

    content added

--- 2k26/jul - 2k26/180726reactpractice/src/App.jsx
+++ 2k26/jul - 2k26/180726reactpractice/src/App.jsx
@@ -1,7 +1,34 @@
 const App = ()=>{
+  const Students = [
+    {
+      name:'dev prasanna',
+      class:12,
+      roll:10
+    },
+    {
+      name:'vrigu',
+      class:11,
+      roll:4
+    },
+    {
+      name:'illanjei',
+      class:11,
+      roll:9
+    }
+  ]
   return(
     <div>
-      
+      {
+        Students.map((student)=>{
+          return(
+            <>
+              <h3>name = {student.name}</h3>
+              <h3>class = {student.class}</h3>
+              <h3>roll = {student.roll}</h3>
+            </>
+          )
+        })
+      }
     </div>
   )
 }
