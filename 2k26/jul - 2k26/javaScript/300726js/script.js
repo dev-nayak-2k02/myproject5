5170a969c1e5b85e7e1b4fb2f6d8c574f8206f49
dev nayak10 <devprasannanayak@gmail.com>
2026-09-04 17:50:41 +0530

    content added

---
+++ 2k26/jul - 2k26/javaScript/300726js/script.js
@@ -0,0 +1,58 @@
+// let user = 'rock';
+// let comp = 'paper';
+// function game(user, comp){
+//     if(user === comp) return 'draw';
+//     if(user === 'scissor' && comp === 'paper') return 'user';
+//     if(user === 'rock' && comp === 'scissor') return 'user';
+//     if(user === 'paper' && comp === 'rock') return 'user';
+//     return 'computer';
+// }
+// console.log(game(user, comp))
+
+
+
+// let i = 1;
+// while(i<16){
+//     if(i%2 !== 0){
+//         console.log(i)
+//     }
+//     i++;
+// }
+
+
+
+// for(let i = 5; i<=50; i=i+5){
+//     console.log(i);
+// }
+
+
+
+// let sum = 0;
+// for(let i = 1; i<=100; i++){
+//     sum += i;
+// }
+// console.log(sum);
+
+
+
+// for(let i = 1; i<=50; i++){
+//     if(i%3 === 0){
+//         console.log(i);
+//     }
+// }
+
+
+
+// let num = Number(prompt('Enter a number: '));
+// for(i=1; i<num+1; i++){
+//     if(i%2 === 0) {
+//         console.log('even',i);
+//     }
+//     else {
+//         console.log('odd',i)
+//     }
+// }
+
+
+
+
